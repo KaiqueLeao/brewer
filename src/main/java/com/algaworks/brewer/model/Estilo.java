3fdc52917dbca1d5ec3acd1b51615b05cc12a67c
Kaique Leão <kaiqueleaodeveloper@gmail.com>
2017-04-20 00:02:02 -0300

    Cria cadastro de estilos com validação

--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -3,13 +3,16 @@ package com.algaworks.brewer.model;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(name = "estilo")
@@ -21,6 +24,8 @@ public class Estilo implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
 	
+	@NotBlank(message = "Campo nome obrigatório")
+	@Size(max = 15, message = "A quantidade de caractéres do campo nome deve ser menor que 15")
 	private String nome;
 	
 	@OneToMany(mappedBy = "estilo")
