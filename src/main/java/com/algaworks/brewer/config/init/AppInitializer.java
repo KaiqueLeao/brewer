257515d465b42ff785444a56fe69bf1ae3ceae3b
Mateusleao99 <mateus.san.tos@hotmail.com>
2017-06-29 21:24:07 -0300

    Adding controller to upload files

--- src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
+++ src/main/java/com/algaworks/brewer/config/init/AppInitializer.java
@@ -1,6 +1,8 @@
 package com.algaworks.brewer.config.init;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -36,6 +38,11 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 		chracterEncondingFilter.setForceEncoding(true);
 		
 		return new Filter[] { chracterEncondingFilter };
+	}
+	
+	@Override
+	protected void customizeRegistration(Dynamic registration) {
+		registration.setMultipartConfig(new MultipartConfigElement(""));
 	}
 
 }
