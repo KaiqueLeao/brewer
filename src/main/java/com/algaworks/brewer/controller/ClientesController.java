e8dc77497608e2c91871d90991e97bd00c8fa9d8
Kaique Leão <kaiqueleaodeveloper@gmail.com>
2018-07-14 00:54:54 -0300

    improving radio button implementation

--- src/main/java/com/algaworks/brewer/controller/ClientesController.java
+++ src/main/java/com/algaworks/brewer/controller/ClientesController.java
@@ -2,13 +2,21 @@ package com.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.algaworks.brewer.model.TipoPessoa;
 
 @Controller
+@RequestMapping("/clientes")
 public class ClientesController {
 
-	@RequestMapping("/clientes/novo")
-	public String novo(){
-		return "cliente/CadastroCliente";
+	@RequestMapping("/novo")
+	public ModelAndView novo(){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		mv.setViewName("cliente/CadastroCliente");
+		
+		return mv;
 	}
 	
 }
